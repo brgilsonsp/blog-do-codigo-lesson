e96fdd111fb2542bbf9202b44539f22624985d36
Gilson Souza <br.gilson.sp@gmail.com>
2021-02-17 10:31:08 -0300

    configure redis

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const app = require('./app');
-const port = process.env.PORT_SERVER || 3100;
+require('./redis/blacklist');
 const db = require('./database');
-
 const routes = require('./rotas');
+
+const port = process.env.PORT_SERVER || 3100;
 routes(app);
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
